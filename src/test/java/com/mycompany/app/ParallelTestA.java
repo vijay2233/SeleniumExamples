96ba2dfc05d7b5445e18b9b63edc610fbc84ac57
Reddy Sykam <venkata.reddy.sykam@accenture.com>
2018-05-03 14:06:15 +0530

    updating file

---
+++ src/test/java/com/mycompany/app/ParallelTestA.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParallelTestA {
+	
+	public static RemoteWebDriver driver;
+	public static String appURL = "http://www.google.com";
+	
+	@BeforeClass
+	@Parameters({ "browser" })
+	public void setUp(String browser) throws MalformedURLException {
+		System.out.println("*******************");
+		driver = Browser.getDriver(browser);
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void testGooglePageTitleInIE() {
+		driver.navigate().to("http://www.google.com");
+		String strPageTitle = driver.getTitle();
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing IE browser");
+			driver.quit();
+		}
+	}
+	
+}
