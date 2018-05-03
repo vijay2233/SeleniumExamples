aab6bd1481173608346eb62c1dc3654d35bab128
venkatasykam <venkatasykam@yahoo.com>
2018-05-03 13:47:28 +0530

    Update TestingwBrowserversions.java

--- src/test/java/com/mycompany/app/TestingwBrowserversions.java
+++ src/test/java/com/mycompany/app/TestingwBrowserversions.java
@@ -11,6 +11,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+
+import org.openqa.selenium.TakesScreenshot;
+
+
 public class TestingwBrowserversions {
 
 	@Test
@@ -30,5 +39,24 @@ public class TestingwBrowserversions {
 
 		//driver.quit();
 	}
+	public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+
+        //Convert web driver object to TakeScreenshot
+
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+
+        //Call getScreenshotAs method to create image file
+
+                File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+            //Move image file to new destination
+
+                File DestFile=new File(fileWithPath);
+
+                //Copy file at destination
+
+                FileUtils.copyFile(SrcFile, DestFile);
+
+    }
 }
 
