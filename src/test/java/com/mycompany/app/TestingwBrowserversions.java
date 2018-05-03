05445da12f4269bb85a750b613202c365d142e29
Reddy Sykam <venkata.reddy.sykam@accenture.com>
2018-05-03 13:27:55 +0530

    updatedt the test case names

--- src/test/java/com/mycompany/app/TestingwBrowserversions.java
+++ src/test/java/com/mycompany/app/TestingwBrowserversions.java
@@ -1,21 +1,33 @@
-package com.pack;
+package javacodegeeks.seleniumHeadlessBrowser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
-public class GoogleSearchTest {
+public class TestingwBrowserversions {
 
 	@Test
-	public void googleTitleSearch() {
+	public void testEasy() {
 		System.setProperty("webdriver.chrome.driver", "C://Users/venkata.reddy.sykam/selemium/nodes/chromedriver.exe");
 		WebDriver driver  = new ChromeDriver();
 		driver.navigate().to("http://google.com");
 		//driver.navigate().to("http://localhost:8080/WebApp-1.0.0-SNAPSHOT/sample.html");
 		String appTitle = driver.getTitle();
 		System.out.println("Application title is :: "+appTitle);
-		Assert.assertTrue(appTitle.equalsIgnoreCase("Googles"), "Page title does not match");
+		WebElement java = driver.findElement(By.name("q"));
+		java.sendKeys("Java Code Geeks");
+		java.submit();
+
+		System.out.println("Title of the page now is " + driver.getTitle());
+
 		//driver.quit();
 	}
 }
+
